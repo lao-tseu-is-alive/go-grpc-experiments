dbf4a84500a238a1b715f1c4c81deba60bc9b8c7
cgil <lao.tseu.is.alive@gmail.com>
2023-04-14 11:52:19 +0200

    added TLS with openssl self-signed certs

--- remoteCalculator/client/main.go
+++ remoteCalculator/client/main.go
@@ -6,27 +6,57 @@ import (
 	"fmt"
 	pb "github.com/lao-tseu-is-alive/go-grpc-experiments/remoteCalculator/calc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"log"
+	"os"
 	"time"
 )
 
-const defaultPort = 50051
+const (
+	VERSION              = "0.1.1"
+	APP                  = "go-grpc-calcServer"
+	baseSslPath          = "openssl_certificates/"
+	defaultPort          = 50051
+	defaultServerIp      = "localhost"
+	useTLS               = true
+	certificateAuthority = "ca.crt"
+	defaultTimeoutSecond = 3
+	defaultAuthTOKEN     = "14d3d4de9273fb1b5cc33e6bae2225f115885d028ddfb82555172188f9dee91c"
+)
 
 // main is client to call the gRPC calc server, just try this from your shell :  go run remoteCalculator/client/main.go -num1 2 -num2 2 -operation M
 func main() {
 	port := flag.Int("port", defaultPort, "the address to connect to")
+	serverIP := flag.String("server-ip", defaultServerIp, "The server Ip address")
 	num1 := flag.Float64("num1", 0, "first operand number")
 	num2 := flag.Float64("num2", 0, "second operand number")
 	op := flag.String("operation", "Add", "Operation to apply to the numbers [Add|Subtract|Multiply|Divide]")
 	flag.Parse()
-	addr := fmt.Sprintf("127.0.0.1:%d", *port)
-	//	127.0.0.1:50051
-	log.Printf("Will try to connect to : %s", addr)
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	l := log.New(os.Stdout, fmt.Sprintf("%s ", APP), log.Ldate|log.Ltime|log.Lshortfile)
+
+	listenAddr := fmt.Sprintf("%s:%d", *serverIP, *port)
+	l.Printf("INFO: 'Starting %s version:%s. Will try to connect to %s'", APP, VERSION, listenAddr)
+	var opts []grpc.DialOption
+	if useTLS {
+		certAuth := baseSslPath + certificateAuthority
+		l.Printf("INFO: 'TLS enabled CA certAuth: %s'", certAuth)
+		sslCredentials, err := credentials.NewClientTLSFromFile(certAuth, "")
+		if err != nil {
+			l.Printf("INFO: for TLS to work you should have your certificates in directory : %s", baseSslPath)
+			l.Printf("INFO: you can generate self-signed certificates using : scripts/make_ssl_certificates.sh")
+			l.Fatalf("💥💥 error loading CA certificate: %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(sslCredentials))
+	} else {
+		l.Println("INFO: TLS is disabled !")
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+	// Set up a connection to the server allowing insecure (no ssl).
+	conn, err := grpc.Dial(listenAddr, opts...)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		l.Fatalf("💥💥 ERROR: unable to connect doing grpc.Dial(%s) : %v", listenAddr, err)
 	}
 	defer conn.Close()
 	c := pb.NewCalcServiceClient(conn)
@@ -46,16 +76,23 @@ func main() {
 	}
 
 	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeoutSecond*time.Second)
 	defer cancel()
-	log.Printf("Sending num1: %.3f , num2: %.3f and operation :%s", *num1, *num2, calcOp.String())
+	// Anything linked to this variable will transmit request headers.
+	md := metadata.New(map[string]string{"x-request-id": "req-123"})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	// Anything linked to this variable will fetch response headers.
+	var header metadata.MD
+	l.Printf("INFO: will send gRPC request Calc(num1: %.3f , num2: %.3f, Operation :%s)", *num1, *num2, calcOp.String())
 	r, err := c.Calc(ctx, &pb.CalcRequest{
 		Num1:      *num1,
 		Num2:      *num2,
 		Operation: calcOp,
-	})
+	},
+		grpc.Header(&header))
 	if err != nil {
-		log.Fatalf("Unable to call gRPC Calc() error: %v", err)
+		l.Fatalf("💥💥 ERROR: failed call gRPC Calc() : %v", err)
 	}
-	log.Printf("Server sended response  result: %v error:%v", r.GetResult(), r.GetError())
+	l.Printf("INFO: server response is result: %v  with error:%v", r.GetResult(), r.GetError())
 }
--- remoteCalculator/server/main.go
+++ remoteCalculator/server/main.go
@@ -2,21 +2,42 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"flag"
 	"fmt"
 	pb "github.com/lao-tseu-is-alive/go-grpc-experiments/remoteCalculator/calc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
+	"os"
 )
 
-// calcServer is used to implement calc Server
+const (
+	VERSION          = "0.1.1"
+	APP              = "go-grpc-calcServer"
+	baseSslPath      = "openssl_certificates/"
+	defaultPort      = 50051
+	defaultServerIp  = "localhost"
+	useTLS           = true
+	serverCrt        = "localhost_server.crt"
+	serverPem        = "localhost_server.pem"
+	defaultTokenSize = 32
+)
+
+// calcServer is used to implement calc gRPC Server
 type calcServer struct {
 	pb.UnimplementedCalcServiceServer
+	l     *log.Logger
+	token string
 }
 
+// Calc handles the gRPC CalcRequest by doing basic math operation on num1 and num2, it then returns the result in CalcResponse
 func (s *calcServer) Calc(ctx context.Context, in *pb.CalcRequest) (*pb.CalcResponse, error) {
-	log.Printf("Received: num1: %v , num2: %v ,  operation : %v", in.GetNum1(), in.GetNum2(), in.GetOperation())
+	s.l.Printf("TRACE: entering Calc(num1: %v , num2: %v ,  operation : %v)", in.GetNum1(), in.GetNum2(), in.GetOperation())
 
 	var res float64
 	isError := false
@@ -44,17 +65,89 @@ func (s *calcServer) Calc(ctx context.Context, in *pb.CalcRequest) (*pb.CalcResp
 	}, nil
 }
 
-func main() {
-	port := flag.Int("port", 50051, "The server port")
-	flag.Parse()
-	l, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+func TokenGenerator(l int) string {
+	b := make([]byte, l)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+// LogInterceptor allows to intercept any request done to the gRPC server
+func LogInterceptor(l *log.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		l.Printf("TRACE: entering LogInterceptor() received request:\n%+v\n", req)
+
+		headers, ok := metadata.FromIncomingContext(ctx)
+		if ok {
+			l.Printf("# LogInterceptor() Received headers: \n%+v\n", headers)
+		}
+		return handler(ctx, req)
 	}
-	//var opts []grpc.ServerOption
-	//grpcServer := grpc.NewServer(opts...)
-	grpcServer := grpc.NewServer()
-	pb.RegisterCalcServiceServer(grpcServer, &calcServer{})
-	log.Printf("gRPC Calc Server listening at %v", l.Addr())
-	grpcServer.Serve(l)
+}
+
+func CheckHeaderInterceptor(l *log.Logger, token string) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		l.Printf("TRACE: entering CheckHeaderInterceptor() ctx:\n %+v\n", ctx)
+		headers, ok := metadata.FromIncomingContext(ctx)
+
+		if !ok {
+			l.Printf("🔥🔥 Error: reading metadata.FromIncomingContext(ctx:\n %+v)\n", ctx)
+			return nil, status.Error(codes.Internal, "Error while reading the context")
+		}
+
+		if len(headers.Get("authorization")) == 0 {
+			l.Println("🤔🤔 WARN: in CheckHeaderInterceptor did not find authorization header !")
+			// return nil, status.Error(codes.Unauthenticated, "Expected authorization header")
+			//for now just accept this unauthorized call :-)
+			return handler(ctx, req)
+		}
+
+		//TODO : also add check for valid token when authorization header was found
+
+		return handler(ctx, req)
+	}
+}
+
+func main() {
+	port := flag.Int("port", defaultPort, "The server port")
+	serverIP := flag.String("server-ip", defaultServerIp, "The server Ip address")
+	flag.Parse()
+	l := log.New(os.Stdout, fmt.Sprintf("%s ", APP), log.Ldate|log.Ltime|log.Lshortfile)
+	listenAddr := fmt.Sprintf("%s:%d", *serverIP, *port)
+	l.Printf("INFO: 'Starting %s version:%s server on %s'", APP, VERSION, listenAddr)
+
+	rootToken := TokenGenerator(defaultTokenSize)
+
+	netListener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		l.Fatalf("💥💥 error doing net.Listen(%s) : %v", listenAddr, err)
+	}
+	var opts []grpc.ServerOption
+	if useTLS {
+		certFile := baseSslPath + serverCrt
+		l.Printf("INFO: 'TLS enabled, certFile: %s'", certFile)
+		keyFile := baseSslPath + serverPem
+		l.Printf("INFO: 'TLS enabled  keyFile: %s'", keyFile)
+		sslCredentials, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+
+		if err != nil {
+			l.Printf("INFO: for TLS to work you should have your certificates in directory : %s", baseSslPath)
+			l.Printf("INFO: you can generate self-signed certificates using : scripts/make_ssl_certificates.sh")
+			l.Fatalf("💥💥 error loading certificates: %v", err)
+		}
+		opts = append(opts, grpc.Creds(sslCredentials))
+	}
+
+	opts = append(opts, grpc.ChainUnaryInterceptor(LogInterceptor(l), CheckHeaderInterceptor(l, rootToken)))
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterCalcServiceServer(grpcServer, &calcServer{
+		UnimplementedCalcServiceServer: pb.UnimplementedCalcServiceServer{},
+		l:                              l,
+		token:                          rootToken,
+	})
+	l.Printf("INFO: 'will start server %s'", listenAddr)
+	err = grpcServer.Serve(netListener)
+	if err != nil {
+		l.Fatalf("💥💥 error doing grpcServer.Serve : %v", err)
+	}
 }
